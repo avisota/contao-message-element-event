620fde79be182dd7f6bc89d44a946f528f97e23d
Tristan Lins <tristan.lins@bit3.de>
2014-05-15 15:15:11 +0200

    Add invisible field to palette.

--- module/dca/orm_avisota_message_content.php
+++ module/dca/orm_avisota_message_content.php
@@ -20,9 +20,10 @@
  */
 $GLOBALS['TL_DCA']['orm_avisota_message_content']['metapalettes']['event'] = array
 (
-	'type'    => array('type', 'cell', 'headline'),
-	'include' => array('eventId', 'eventTemplate'),
-	'expert'  => array(':hide', 'cssID', 'space')
+	'type'      => array('type', 'cell', 'headline'),
+	'include'   => array('eventId', 'eventTemplate'),
+	'expert'    => array(':hide', 'cssID', 'space'),
+	'published' => array('invisible'),
 );
 
 $GLOBALS['TL_DCA']['orm_avisota_message_content']['fields']['eventId']       = array
