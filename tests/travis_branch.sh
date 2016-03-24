38fa6b5ebcf6996a3fe3fafb9bccc0b7fb13b221
Sven Baumann <baumann.sv@gmail.com>
2016-03-24 17:25:11 +0100

    inject bash commands for travis before install in travis_branch.sh

--- tests/travis_branch.sh
+++ tests/travis_branch.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
-echo test
+if [ ${TRAVIS_BRANCH} == "master" ]; then
+    export COMPOSER_ROOT_VERSION=dev-develop;
+elif [ ${TRAVIS_BRANCH} == "develop" ]; then
+    export COMPOSER_ROOT_VERSION=dev-develop;
+elif [ ${TRAVIS_BRANCH} == *"release"* ]; then
+    export COMPOSER_ROOT_VERSION=dev-${TRAVIS_BRANCH};
+else
+    export COMPOSER_ROOT_VERSION=${TRAVIS_BRANCH};
+fi;
 
-echo ${TRAVIS_BRANCH}
+echo COMPOSER_ROOT_VERSION=${COMPOSER_ROOT_VERSION}
 
-echo test
