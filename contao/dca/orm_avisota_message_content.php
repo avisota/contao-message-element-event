9a5f9c57069bd9b3e3b9dad528a7bea6cd91e474
Sven Baumann <baumann.sv@gmail.com>
2016-11-18 13:08:42 +0100

    add by selectri widget the configuration for canonical

--- contao/dca/orm_avisota_message_content.php
+++ contao/dca/orm_avisota_message_content.php
@@ -34,6 +34,7 @@ $GLOBALS['TL_DCA']['orm_avisota_message_content']['fields']['eventIdWithTimestam
         'min'  => 1,
         'max'  => 99,
         'data' => 'Avisota\Contao\Message\Element\Event\DataContainer\EventListDataFactory',
+        'canonical' => true
     ),
     'field'     => array(
         'type'     => 'array',
